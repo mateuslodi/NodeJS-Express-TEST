6a35b2b356f795a9050e8e22d7a773113fbc4ef3
Mateus Camargo Lodi <mateus718@gmail.com>
2022-07-31 21:10:23 -0300

    second save express

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const { randomUUID } = require("crypto");
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json({success:true, message:"Esta é minha página principal"});
+const port = 3000;
+const AgentsList = [];
+
+app.get('/', (request, response) => {
+    request.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
+    response.json({success:true, message:"Esta é minha página principal"});
 })
+
+app.post("/agents", (request, response) => {
+  const { name, role, firstAbility } = request.body;
+  
+  const product = {
+    name,
+    role,
+    firstAbility,
+    id: randomUUID()
+  }
+
+  AgentsList.push(product);
+
+  console.log(AgentsList);
+  response.json({success:true});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} with nodemon`);
