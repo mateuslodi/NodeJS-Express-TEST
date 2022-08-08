597af42c406d5f3d121898fe334f7118664fedb1
Mateus Camargo Lodi <mateus718@gmail.com>
2022-08-07 22:28:29 -0300

    third commit

--- app.js
+++ app.js
@@ -9,24 +9,32 @@ const AgentsList = [];
 app.get('/', (request, response) => {
     request.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
-    response.json({success:true, message:"Esta é minha página principal"});
+    response.json({success:true, message:"This is my home page"});
 })
 
 app.post("/agents", (request, response) => {
   const { name, role, firstAbility } = request.body;
   
-  const product = {
+  const agent = {
     name,
     role,
     firstAbility,
     id: randomUUID()
   }
 
-  AgentsList.push(product);
+  AgentsList.push(agent);
 
-  console.log(AgentsList);
-  response.json({success:true});
+  response.json({success:true, agentList: AgentsList});
 });
+
+app.get('/agents/:id', (request, response) => {
+  const { id } = request.params;
+  const agent = AgentsList.find(AgentsList => AgentsList.id === id);
+  if(agent !== undefined) {
+    response.json({success:true, agent: agent});
+  }
+  response.json({success:false, message: "No agent was found with this id"});
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} with nodemon`);
